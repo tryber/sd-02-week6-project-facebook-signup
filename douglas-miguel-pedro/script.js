a75804a35c2a62be7b06bb9373aeec14283b4ff5
Douglas Henrique <doughrqe@gmail.com>
2019-12-09 17:03:02 -0300

    Utilizando bootstrap para estilizar forms

---
+++ douglas-miguel-pedro/script.js
@@ -0,0 +1,12 @@
+let picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    toString(date, format) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    firstDay: 1,
+    maxDate: new Date(2022, 12, 31),
+    yearRange: [2000, 2020]
+});
