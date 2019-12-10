b2a7f677f729fee06e4974f35c0576f886ac77b7
Douglas Henrique <doughrqe@gmail.com>
2019-12-09 21:15:29 -0300

    code climate

--- douglas-miguel-pedro/script.js
+++ douglas-miguel-pedro/script.js
@@ -1,17 +1,22 @@
-let picker = new Pikaday({
-    field: document.getElementById('datepicker'),
-    toString(date, format) {
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        return `${day}/${month}/${year}`;
-    },
-    firstDay: 1,
-    maxDate: new Date(2022, 12, 31),
-    yearRange: [2000, 2020]
+new Pikaday({
+  field: document.getElementById('datepicker'),
+  toString(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  firstDay: 1,
+  maxDate: new Date(2022, 12, 31),
+  yearRange: [2000, 2020],
 });
 
-let botaoAzul = document.getElementsByClassName('azul')[0];
+const botaoAzul = document.getElementsByClassName('azul')[0];
 botaoAzul.addEventListener('click', function () {
-    alert('ola')
+  customAlert("Something happened!");
 })
+
+function foo() {
+  var alert = myCustomLib.customAlert;
+  alert();
+}
