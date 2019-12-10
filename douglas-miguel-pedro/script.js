dc70ff25f9371cd02336867c34f1601fc173d835
Douglas Henrique <doughrqe@gmail.com>
2019-12-10 15:55:47 -0300

    code climate

--- douglas-miguel-pedro/script.js
+++ douglas-miguel-pedro/script.js
@@ -2,20 +2,19 @@
 const regexNome = new RegExp(/[a-zA-Z]/);
 const regexTelefone = new RegExp(/[1-9]{11}/);
 const regexEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-function escolheDia() {
-  Pikaday({
-    field: document.getElementById('datepicker'),
-    toString(date) {
-      const day = date.getDate();
-      const month = date.getMonth() + 1;
-      const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
-    },
-    firstDay: 1,
-    maxDate: new Date(2019, 11, 31),
-    yearRange: [1905, 2020],
-  });
-}
+const date = new Pikaday({
+  field: document.getElementById('datepicker'),
+  toString(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  firstDay: 1,
+  maxDate: new Date(2019, 11, 31),
+  yearRange: [1905, 2020],
+});
+
 const botaoAzul = document.getElementsByClassName('azul')[0];
 botaoAzul.addEventListener('click', function () {
   const botoesLogin = document.querySelectorAll('.primeirosInputs');
@@ -35,7 +34,6 @@ cadastro.addEventListener('click', function () {
   const sobrenome = document.getElementById('sobrenome').value;
   const novasenha = document.getElementById('novasenha').value;
   const emailOuPassword = document.getElementById('emailoupassword').value;
-  const data = document.getElementById('datepicker').value;
   const options = document.querySelectorAll('.option');
   event.preventDefault();
   if (!regexNome.test(nome)) {
@@ -46,15 +44,12 @@ cadastro.addEventListener('click', function () {
     alert('E-mail ou Telefone inválido');
   } else if (novasenha === '') {
     alert('Nova senha inválida');
-  } else if (data === '') {
+  } else if (date === '') {
     alert('Selecione a data');
   } else if (!options[0].checked && !options[1].checked && !options[2].checked) {
     alert('ENTROU NESSA MERDA');
   } else {
     const genero = (document.querySelector('input[name="Gênero"]:checked').value);
-    alert(`nome: ${nome} sobrenome: ${sobrenome} Nova senha: ${novasenha} E-mail/Telefone: ${emailOuPassword} Nascimento: ${data} Gênero: ${genero}`);
+    alert(`nome: ${nome} sobrenome: ${sobrenome} Nova senha: ${novasenha} E-mail/Telefone: ${emailOuPassword} Nascimento: ${date} Gênero: ${genero}`);
   }
 });
-window.onload = function () {
-  escolheDia();
-};
