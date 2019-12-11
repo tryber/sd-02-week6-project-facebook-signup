d28edbbb4c9a9f2c2f011fffc62e1ee5598193cf
Fernando Luckesi <fernando.luckesi94@gmail.com>
2019-12-11 17:57:28 -0300

    funçao de validaçao do input email sintax correta

--- giorgia-fernando/script.js
+++ giorgia-fernando/script.js
@@ -1,24 +1,23 @@
-const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const regexTelefone = /\d{4,5}-\d{4}/;
+let regexEmail = /^(([^<>()[\]\\.,;:\s@“]+(\.[^<>()[\]\\.,;:\s@“]+)*)|(“.+“))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
+let regexTelefone = /\d{4,5}-\d{4}/;
 
-function verificandoLogin() {
-  const email = document.getElementById('#email');
-  const senha = document.getElementById('#senha');
-  if (email.value === '' || regexEmail === '') {
+
+
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+let entrar = document.getElementById('botaoEntrar');
+
+entrar.addEventListener('click', function () {
+  if (email.value == '' || regexEmail.test(email.value) /* && email.value != regexTelefone */) {
     alert('Não é possível fazer o login se não inserir um e-mail ou telefone válido.');
-  } else if (senha === '' || regexTelefone === '') {
+  } else if (senha == '') {
     alert('Não é possível fazer o login se não inserir uma senha válida.');
   } else {
     alert('Login realizado com sucesso!');
   }
-}
-
-const entrar = document.getElementById('#botaoEntrar');
-entrar.addEventListener('click', function () {
-  verificandoLogin();
 });
 
-verificandoLogin(); 
+
 // Função abaixo insere o datepicker da biblioteca JQuery
 $(function dataNascimento() {
   $('#calendario').datepicker();
