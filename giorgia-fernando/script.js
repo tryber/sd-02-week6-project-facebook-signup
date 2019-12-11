2dfd690f1215d2f0e09d0c2ccab137c4e8e4e9af
Giorgia <giorgialeoniac@gmail.com>
2019-12-11 01:19:12 -0300

    Enviando arquivos organizados pra ver se o Code Climate fala algo

--- giorgia-fernando/script.js
+++ giorgia-fernando/script.js
@@ -6,24 +6,10 @@ const botaoEntrar = document.querySelector('#botaoEntrar');
 let campoInserido = document.getElementsByTagName('input')[0].value
 
 botaoEntrar.addEventListener('click', function () {
-  console.log(rolou);
-  // event.preventDefault()
-  // if (regexEmail == true || regexTelefone == true) {
-  //   alert("Você não inseriu um e-mail ou número de telefone válido.");
-  // } else {
-  //   alert("Login realizado com sucesso!");
-  // }
+  event.preventDefault()
+  if (regexEmail == true || regexTelefone == true) {
+    alert("Você não inseriu um e-mail ou número de telefone válido.");
+  } else {
+    alert("Login realizado com sucesso!");
+  }
 })
-
-// function dados() {
-//   let dadosParaValidar = {
-//     "nome": document.querySelector("#nome").value,
-//     "sobrenome": document.querySelector("#sobrenome").value,
-//     "email": document.querySelector("#contato").value,
-//     "novaSenha": document.querySelector("#novaSenha").value,
-//     //"dataDeNascimento": document.querySelector(XXX).value,
-//     "genero": document.querySelector("#genero")
-//     "sexo": document.querySelector('input[id='genero']:checked').value,
-//   }
-//   console.log(dados());
-// }
