d1343a85446cfe57993eb261768dc321e9385fd9
Giorgia <giorgialeoniac@gmail.com>
2019-12-11 16:01:25 -0300

    Correçõees no code climate e no js

--- giorgia-fernando/script.js
+++ giorgia-fernando/script.js
@@ -1,31 +1,24 @@
 const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const regexTelefone = /\d{4,5}-\d{4}/g;
+const regexTelefone = /\d{4,5}-\d{4}/;
 
 function verificandoLogin() {
   const email = document.getElementById('#email');
   const senha = document.getElementById('#senha');
-  if (email.value === '') {
-    alert('Não é possível fazer o login se não inserir um e-mail ou telefone válido.')
-  } else if (senha === '') {
-    alert('Não é possível fazer o login se não inserir uma senha válida.')
-  } else {
-    alert('Login realizado com sucesso!')
-  }
-}
-
-verificandoLogin();
-
-let botaoEntrar = document.querySelector('#botaoEntrar');
-
-botaoEntrar.addEventListener('click', function () {
-  event.preventDefault();
-  if (regexEmail === true || regexTelefone === true) {
-    alert('Você não inseriu um e-mail ou número de telefone válido.');
+  if (email.value === '' || regexEmail === '') {
+    alert('Não é possível fazer o login se não inserir um e-mail ou telefone válido.');
+  } else if (senha === '' || regexTelefone === '') {
+    alert('Não é possível fazer o login se não inserir uma senha válida.');
   } else {
     alert('Login realizado com sucesso!');
   }
+}
+
+const entrar = document.getElementById('#botaoEntrar');
+entrar.addEventListener('click', function () {
+  verificandoLogin();
 });
 
+verificandoLogin(); 
 // Função abaixo insere o datepicker da biblioteca JQuery
 $(function dataNascimento() {
   $('#calendario').datepicker();
