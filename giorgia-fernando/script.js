d23e4ab63dc9930d6e503273891da0eaff53c137
Giorgia <giorgialeoniac@gmail.com>
2019-12-11 01:44:23 -0300

    Correções code climate

--- giorgia-fernando/script.js
+++ giorgia-fernando/script.js
@@ -3,13 +3,13 @@ const regexTelefone = /\d{4,5}-\d{4}/g;
 
 
 const botaoEntrar = document.querySelector('#botaoEntrar');
-let campoInserido = document.getElementsByTagName('input')[0].value
+let campoInserido = document.getElementsByTagName('input')[0].value;
 
 botaoEntrar.addEventListener('click', function () {
-  event.preventDefault()
-  if (regexEmail == true || regexTelefone == true) {
-    alert("Você não inseriu um e-mail ou número de telefone válido.");
+  event.preventDefault();
+  if (regexEmail === true || regexTelefone === true) {
+    alert('Você não inseriu um e-mail ou número de telefone válido.');
   } else {
-    alert("Login realizado com sucesso!");
+    alert('Login realizado com sucesso!');
   }
-})
+});
