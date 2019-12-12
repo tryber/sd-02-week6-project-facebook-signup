89ce918401afd49e6968b317683930e03e4cf147
Ishidor <kevinmendessilva@hotmail.com>
2019-12-12 11:17:25 -0300

    Corrigindo Code Climate #1

--- Andrey_Kevin/script.js
+++ Andrey_Kevin/script.js
@@ -1,31 +1,30 @@
 const inputDate = document.getElementById('datepicker');
 
-const picker = new Pikaday({
+new Pikaday({
   field: document.getElementById('datepicker'),
   format: 'D/M/YYYY',
-  toString(date, format) {
-      const day = date.getDate();
-      const month = date.getMonth() + 1;
-      const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
+  toString(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   },
-  parse(dateString, format) {
-      const parts = dateString.split('/');
-      const day = parseInt(parts[0], 10);
-      const month = parseInt(parts[1], 10) - 1;
-      const year = parseInt(parts[2], 10);
-      return new Date(year, month, day);
-  }
+  parse(dateString) {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  },
 });
 
 const nameRegex = new RegExp(/[a-zA-Z]/);
 const phoneRegex = new RegExp(/[0-9]{11}/);
-const emailRegex = new RegExp(/^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+const emailRegex = new RegExp(/^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 const dateRegex = new RegExp(/^\d{1,2}\/\d{1,2}\/\d{4}$/);
 
-const cadastrar = document.getElementById('downBtn')
+const cadastrar = document.getElementById('downBtn');
 cadastrar.addEventListener('click', function () {
-
   const name = document.getElementById('nome').value;
   const lastName = document.getElementById('sobrenome').value;
   const phoneMail = document.getElementById('phonemail').value;
@@ -34,27 +33,19 @@ cadastrar.addEventListener('click', function () {
   const male = document.getElementById('male');
   const personalizado = document.getElementById('personalizado');
 
-
-
-  if (!nameRegex.test(name)){
-    alert ('O nome informado é inválido');
-  }
-  else if (!nameRegex.test(lastName)){
-    alert ('O sobrenome informado é inválido');
-  }
-  else if ((!phoneRegex.test(phoneMail)) && (!emailRegex.test(phoneMail))) {
-    alert ('O telefone/email informado é inválido');
-  }
-  else if (password === ''){
-    alert ('Informe sua senha');
-  }
-  else if (!dateRegex.test(inputDate.value)){
-    alert ('Informe sua data de nascimento');
-  }
-  else if (!female.checked && !male.checked && !personalizado.checked){
-    alert ('Informe o seu gênero');
-  }
-  else {
+  if (!nameRegex.test(name)) {
+    alert('O nome informado é inválido');
+  }  else if (!nameRegex.test(lastName)) {
+    alert('O sobrenome informado é inválido');
+  }  else if ((!phoneRegex.test(phoneMail)) && (!emailRegex.test(phoneMail))) {
+    alert('O telefone/email informado é inválido');
+  }  else if (password === '') {
+    alert('Informe sua senha');
+  }  else if (!dateRegex.test(inputDate.value)) {
+    alert('Informe sua data de nascimento');
+  }  else if (!female.checked && !male.checked && !personalizado.checked){
+    alert('Informe o seu gênero');
+  }  else {
     alert(` Conta criada com sucesso!
             nome: ${name}
             sobrenome: ${lastName}
@@ -62,5 +53,4 @@ cadastrar.addEventListener('click', function () {
             E-mail/Telefone: ${phoneMail}
             Data de nascimento: ${inputDate.input}`);
   }
-
-})
+});
