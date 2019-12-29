bfa3f107e284495919581e701771be254f44ac15
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-12-28 21:45:51 -0300

    Bug Squash - 64 restantes

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -1,6 +1,37 @@
+/* Variáveis globais */
+
+const botaoEnviar = document.querySelector('#enviar');
+const containerRadios = document.querySelector('.gender-block');
+const arrRadios = document.querySelectorAll('.gender-input');
+const divErro = document.createElement('div');
+const divEmail = document.createElement('div');
+const divUndefined = document.querySelector('#show-undefined');
+const inputNome = document.querySelector('#nome');
+const inputSobrenome = document.querySelector('#sobrenome');
+const inputFoneOuEmail = document.querySelector('#fone-ou-email');
+const inputSenha = document.querySelector('#senha');
+const inputDtNasc = document.querySelector('#dtnasc');
+const selectPronome = document.querySelector('.select-pronome');
+const inputGeneroOpt = document.querySelector('#gender-opt');
+const formRegister = document.querySelector('#form-register');
+
+const camposInputForm = [ inputNome, inputSobrenome, inputFoneOuEmail, inputSenha, inputDtNasc, inputGeneroOpt, ]
+const camposObrigInputForm = [ inputNome, inputSobrenome, inputFoneOuEmail, inputSenha, inputDtNasc, ]
+const camposObrigInputName = [ 'Nome', 'Sobrenome', 'Celular ou email', 'Nova senha', 'Data nascimento', ]
+
+let emailErro = '';
+let contadorEmail = 0;
+let contadorGenero = 1;
+let valorGenero = 0;
+let senhaErro = '';
+let contadorSenha = 0;
+let contadorValida = 0;
+let alertaErro = '';
+let alertaOk = '';
+
 /* Pikaday JS - Requisito 17 */
 
-const dataNasc = new Pikaday({
+new Pikaday({
   field: document.getElementById('dtnasc'),
   firstDay: 1,
   minDate: new Date(1899, 12, 1),
@@ -17,60 +48,38 @@ const dataNasc = new Pikaday({
 });
 
 /* Validações com a Lib js-form-validator */
-const formRegister = document.querySelector('#form-register');
-let res;
-const validaCampos = new Validator(formRegister, function (err, res) {
+
+new Validator(formRegister, function (err, res) {
   if (contadorGenero == 1) {
     res = false;
-  };
-  console.log(res);
-  if (valorGenero == 3 && selectPronome.value == '') {
+  }
+  if (valorGenero === 3 && selectPronome.value == '') {
     res = false;
-  };
-  console.log(res);
+  }
   return res;
 }, {
   rules: {
-    date: function (value) {
+    dtval(value) {
       const dataReg = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/([1|2][9|0][0-9][0-9])/;
       if (value.match(dataReg)) {
         return true;
+      }
+      else {
+        return false;
       }
     }
   },
   messages: {
     pt: {
-      date: {
-        incorrect: 'Por favor, digite uma data válida no estilo "DD/MM/AAAA"'
-      }
-    }
-  }
+      dtval: {
+        incorrect: 'Por favor, digite uma data válida no estilo "DD/MM/AAAA"',
+      },
+    },
+  },
 });
 
-/* Variáveis globais */
-const botaoEnviar = document.querySelector('#enviar');
-const containerRadios = document.querySelector('.gender-block');
-const arrRadios = document.querySelectorAll('.gender-input');
-const divErro = document.createElement('div');
-const divEmail = document.createElement('div');
-const divUndefined = document.querySelector('#show-undefined');
-
-const inputNome = document.querySelector('#nome');
-const inputSobrenome = document.querySelector('#sobrenome');
-const inputFoneOuEmail = document.querySelector('#fone-ou-email');
-const inputSenha = document.querySelector('#senha');
-const inputDtNasc = document.querySelector('#dtnasc');
-const radioFeminino = document.querySelector('#feminino');
-const radioMasculino = document.querySelector('#masculino');
-const radioPersonalizado = document.querySelector('#personalizado');
-const selectPronome = document.querySelector('.select-pronome');
-const inputGeneroOpt = document.querySelector('#gender-opt');
-
-const camposInputForm = [
-  inputNome, inputSobrenome, inputFoneOuEmail, inputSenha, inputDtNasc, inputGeneroOpt
-]
-
 /* Validando radio button */
+
 divErro.className = 'error esconder';
 divErro.setAttribute('data-type', 'validator-error');
 divErro.innerHTML = 'Escolha um gênero. Você poderá alterar quem pode ver isso posteriormente.';
@@ -102,13 +111,11 @@ divEmail.setAttribute('data-type', 'validator-email');
 divEmail.innerHTML = 'Insira um número de celular ou email válido.<br>Exemplos válidos: 11987876565 ou contato@provedor.com';
 containerEmailEFone = document.querySelector('#fone-email');
 containerEmailEFone.appendChild(divEmail);
-let emailErro = '';
-let contadorEmail = 0;
 
 function funcaoEmailEFone() {
   emailErro = '';
   const emailReg = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
-  const foneReg = /^^[0-9]{2}9?[0-9]{8}$/;
+  const foneReg = /^[0-9]{2}9?[0-9]{8}$/;
   if (inputFoneOuEmail.value != '') {
     if (inputFoneOuEmail.value.match(emailReg) || inputFoneOuEmail.value.match(foneReg)) {
       divEmail.classList.add('esconder');
@@ -124,9 +131,6 @@ function funcaoEmailEFone() {
 inputFoneOuEmail.addEventListener('keyup', funcaoEmailEFone);
 
 /* Validação de senha (acima de 6 caracteres) */
-
-let contadorSenha = 0;
-let senhaErro = '';
 
 function senhaMaisCaracteres() {
   contadorSenha = 0;
@@ -164,13 +168,11 @@ arrRadios.forEach(changeRadio);
 /* Verificar campos vazios e remover placeholder personalizado */
 
 function verificaVazioLimpaPlaceholder(event) {
+  let nomeCampo = event.target.name;
+  escondeLabel = document.querySelector(`label[for=${nomeCampo}]`);
   if (event.target.value == '') {
-    let nomeCampo = event.target.name;
-    escondeLabel = document.querySelector(`label[for=${nomeCampo}]`);
     escondeLabel.classList.remove('esconder');
   } else {
-    let nomeCampo = event.target.name;
-    escondeLabel = document.querySelector(`label[for=${nomeCampo}]`);
     escondeLabel.classList.add('esconder');
   }
 }
@@ -183,18 +185,6 @@ function escutaCampos(item) {
 camposInputForm.forEach(escutaCampos);
 
 /* Criar Alerts */
-let valorGenero = 0;
-let contadorValida = 0;
-let alertaErro = '';
-let alertaOk = '';
-
-const camposObrigInputForm = [
-  inputNome, inputSobrenome, inputFoneOuEmail, inputSenha, inputDtNasc
-]
-
-const camposObrigInputName = [
-  'Nome', 'Sobrenome', 'Celular ou email', 'Nova senha', 'Data nascimento'
-]
 
 function verificaCamposInputVazios() {
   alertaErro = 'Falhas no cadastro, por favor confira os erros abaixo:';
@@ -221,15 +211,14 @@ function verificaSelectVazia() {
   }
 }
 
-let contadorGenero = 1;
-
 function verificaRadiosVazias() {
   contadorGenero = 1;
   for (let i = 0; i < arrRadios.length; i += 1) {
     if (arrRadios[i].checked) {
       contadorGenero = 0;
       valorGenero = arrRadios[i].value;
-      if (valorGenero == 3) {
+      valorGenero = parseInt(valorGenero);
+      if (valorGenero === 3) {
         verificaSelectVazia()
       }
     }
